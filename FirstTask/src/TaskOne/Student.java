00a090209a2ecf0d2e284b8a9fca14ee2221151b
asadgits <iamasadiqbal@gmail.com>
2021-07-15 21:33:57 +0500

    Task1 : Initial commit

---
+++ FirstTask/src/TaskOne/Student.java
@@ -0,0 +1,20 @@
+package TaskOne;
+
+public class Student {
+
+    public String Name ;
+    public String fName ;
+    public String RollNo ;
+
+
+    public  Student(){};
+
+    public  Student(String name , String fname , String rollno){
+        Name = name;
+        fName = fname ;
+        RollNo = rollno;
+
+    }
+
+
+}
