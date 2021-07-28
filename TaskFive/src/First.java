b5a56347f27657124e82b121d04890253f91112e
asadgits <iamasadiqbal@gmail.com>
2021-07-28 20:07:06 +0500

    afer Check task5 is OK and some techniquies must be learned

--- TaskFive/src/First.java
+++ TaskFive/src/First.java
@@ -6,31 +6,29 @@ public class First extends Thread{
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                throw new RuntimeException(e);
+                // e.printStackTrace();
             }
         }
     }
+
     public static void main(String[] args) throws InterruptedException {
 
-        Thread t1 = new Thread(() -> {
-            countDown();
-        });
+        //Method reference
+        Thread t1 = new Thread(First::countDown);
         t1.setName("Thread1");
         t1.start();
         t1.join();
 
-
-        Thread t2 =new Thread(() -> {
-            countDown();
-        });
+        //lambda type
+        Thread t2 = new Thread(()->countDown());
         t2.setName("Thread2");
         t2.start();
         t2.join();
 
-
-        for (int i = 0; i <= 5; i++) {
+        for (int i = 0; i <= 5; i++)
             System.out.println(Thread.currentThread().getName());
-        }
+
 
 
     }//main bracket
