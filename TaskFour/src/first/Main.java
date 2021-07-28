9e01e12d94306e2493e4c4b5e6278bc6ff43b3f7
asadgits <iamasadiqbal@gmail.com>
2021-07-28 16:56:24 +0500

    commiting task4 after completion

--- TaskFour/src/first/Main.java
+++ TaskFour/src/first/Main.java
@@ -2,6 +2,20 @@ package first;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("pakistan");
+
+//        Object ref = null;
+//        ref.toString();
+
+
+       /* StringBuilder sb = null;
+        System.out.println(sb.length());*/
+
+//        String value = null;
+//        if(value.equals("abc")){
+//            System.out.println("nullpointerException");
+//        }
+
+        System.out.println(Task.customExceptionCheck("khalid"));
+
     }
 }
