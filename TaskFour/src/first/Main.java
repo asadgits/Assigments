924dadfd2a672ed8ba4049f6b66a793753dcf65e
asadgits <iamasadiqbal@gmail.com>
2021-07-29 14:21:38 +0500

    TaskFour folder commiting

---
+++ TaskFour/src/first/Main.java
@@ -0,0 +1,21 @@
+package first;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Object ref = null;
+//        ref.toString();
+
+
+       /* StringBuilder sb = null;
+        System.out.println(sb.length());*/
+
+//        String value = null;
+//        if(value.equals("abc")){
+//            System.out.println("nullpointerException");
+//        }
+
+        System.out.println(Task.customExceptionCheck("khalid"));
+
+    }
+}
